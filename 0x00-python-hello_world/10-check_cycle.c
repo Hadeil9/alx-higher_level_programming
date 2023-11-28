1fe3c1274ecafddf6d1b6508d50ef1500502a4d8
Hadeil9 <HadeilOmer1996@gmail.com>
2023-11-27 16:50:54 -0800

    ok

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include <stdlib>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if list is cyelical
+ * @list: pointer to list check
+ * Return: 1 if cyelical, 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
